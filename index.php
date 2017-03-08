25ab73fb7f24dc637e35e4f0f440ffeb6b541e3d
DigitalData <legoxavierlocketravers@gmail.com>
2017-03-08 19:09:08 +0700

    Added google logos.
    Fixed google login

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="css/stylesheet.css" media="screen">
     <title>Honor Pass @ JIS</title>
     <link href="favicon.ico" rel="icon" type="image/x-icon">
+    <?php include 'GoogleLogin.php' ?>
   </head>
 
   <body>
@@ -12,6 +13,22 @@
       <h1>Honor Pass JIS Website</h1>
     </div>
 
+
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+    <script type="text/javascript">
+    function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+
+    }
+    </script>
 
 
     <div id="bottom">
